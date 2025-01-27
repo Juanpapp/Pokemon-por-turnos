711c244f1291fee09db36588c0d20e589db66d36
Juan Pablo Parra Pacheco <juanpacheco2017@gmail.com>
2025-01-28 05:14:43 +0800

    retiro de comentarios

--- pokemon.js
+++ pokemon.js
@@ -2,32 +2,32 @@ var userHP = 100;
 var opHP = 100;
 opAttacks = [flameThrower, dragonClaw, ember, growl];
 playerMove = 0;
-/* movimientos del usuario */
+
 function waterCannon() {
   if(playerMove == 0 && userHP != 0) {
-    var miss = Math.floor((Math.random() * 10) + 1); // tasa de fallos
+    var miss = Math.floor((Math.random() * 10) + 1); 
     if(miss == 1) {
       document.getElementById('message').innerHTML = " ¡El ataque de Blastoise falló! ";
     }
     else {
-      document.getElementById('message').innerHTML = " Blastoise usó el cañón de agua "; // ataque
-      var critical = Math.floor((Math.random() * 10) + 1); // crítico
+      document.getElementById('message').innerHTML = " Blastoise usó el cañón de agua "; 
+      var critical = Math.floor((Math.random() * 10) + 1); 
       if(critical == 4){
         for(var x = 0; x < 2; x++){
-          opHP = opHP - 30; // sí crítico
+          opHP = opHP - 30; 
         }
       }
       else{
-        opHP = opHP - 30; // no crítico
+        opHP = opHP - 30; 
       }
-      if(opHP < 0){ opHP = 0} // desmayo
-        document.getElementById('apHP').innerHTML = opHP; // actualizar hp
+      if(opHP < 0){ opHP = 0} 
+        document.getElementById('apHP').innerHTML = opHP; 
       if(opHP == 0){
-        document.getElementById('message').innerHTML = " ¡Charizard se desmayó! " // desmayo
+        document.getElementById('message').innerHTML = " ¡Charizard se desmayó! " 
       }
     }
-    //wait();
-    playerMove = 1; // actualizar movimiento
+    
+    playerMove = 1; 
 }
 }
 
@@ -50,19 +50,19 @@ function waterPulse() {
       }
     if(opHP < 0 ) { opHP = 0}
     document.getElementById('apHP').innerHTML = opHP;
-    //document.getElementById('message').innerHTML = " Charizard2 "
+    
     if(opHP == 0){
       document.getElementById('message').innerHTML = " ¡Charizard se desmayó! "
     }
   }
-  //wait();
+  
   playerMove = 1;
 }
 }
 
 function surf() {
   if(playerMove == 0 && userHP != 0) {
-  //alert("Water Cannon!");
+  
   var miss = Math.floor((Math.random() * 10) + 1);
   if(miss == 1 ) {
     document.getElementById('message').innerHTML = " ¡Falló el ataque de Blastoise! "
@@ -84,13 +84,13 @@ function surf() {
       document.getElementById('message').innerHTML = " ¡Charizard se desmayó! "
     }
   }
-  //wait();
+  
   playerMove = 1;
 }
 }
 function tackle() {
   if(playerMove == 0 && userHP != 0) {
-  //alert("Water Cannon!");
+  
   var miss = Math.floor((Math.random() * 10) + 1);
   if(miss == 1 ) {
     document.getElementById('message').innerHTML = " ¡Falló el ataque de Blastoise! "
@@ -108,40 +108,40 @@ function tackle() {
       }
     if(opHP < 0){ opHP = 0}
     document.getElementById('apHP').innerHTML = opHP;
-  //document.getElementById('message').innerHTML = " Charizard4 "
+  
     if(opHP == 0){
       document.getElementById('message').innerHTML = " ¡Charizard se desmayó! "
     }
   }
-  //wait();
+  
   playerMove = 1;
 }
 }
 
 
 
-/* movimientos del oponente */
+
 
 function flameThrower() {
-  var miss = Math.floor((Math.random() * 10) + 1); // tasa de fallos
+  var miss = Math.floor((Math.random() * 10) + 1); 
   if(miss == 1 ) {
-  document.getElementById('message').innerHTML = " ¡El ataque de Charizard falló! " // Ataque fallido
+  document.getElementById('message').innerHTML = " ¡El ataque de Charizard falló! " 
   }
   else {
-  document.getElementById('message').innerHTML = " Charizard usó lanzallamas " // ataque
-    var critical = Math.floor((Math.random() * 10) + 1); // critico
+  document.getElementById('message').innerHTML = " Charizard usó lanzallamas " 
+    var critical = Math.floor((Math.random() * 10) + 1); 
       if(critical == 4){
-        for(var x = 0; x < 2; x++){ // si es critico
+        for(var x = 0; x < 2; x++){ 
           userHP = userHP - 30;
         }
       }
       else{
-        userHP = userHP - 30; // no es critico
+        userHP = userHP - 30; 
       }
-  if(userHP < 0) { userHP = 0} // débil
-  document.getElementById('myHP').innerHTML = userHP; // Actualizar hp
-    if(userHP == 0) { // fainted
-      document.getElementById('message').innerHTML = " ¡Blastoise se desmayó! " // se desmayó
+  if(userHP < 0) { userHP = 0} 
+  document.getElementById('myHP').innerHTML = userHP; 
+    if(userHP == 0) { 
+      document.getElementById('message').innerHTML = " ¡Blastoise se desmayó! " 
     }
   }
 }
@@ -220,10 +220,10 @@ function growl() {
 
 
 
-function compPokemon() { // continuar
-  if(playerMove == 1 && opHP != 0) { // ¿Quién se mueve?
-  var move = Math.floor((Math.random() * 4) + 1); // Elige mover aleatoriamente
-    opAttacks[move](); // Ataque de llamada desde la matriz
-    playerMove = 0; // Actualizar movimiento del jugador
+function compPokemon() { 
+  if(playerMove == 1 && opHP != 0) { 
+  var move = Math.floor((Math.random() * 4) + 1); 
+    opAttacks[move](); 
+    playerMove = 0; 
   }
 }
